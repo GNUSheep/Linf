4f50eff9ca466c812f5022f27a5d44a7377a6711
Qiu <qiu@qiu.news>
2020-03-12 20:04:58 +0100

    test main.go

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"runtime"
+
+	"gopkg.in/teh-cmc/go-sfml.v24/graphics"
+	"gopkg.in/teh-cmc/go-sfml.v24/window"
+)
+
+func init() { runtime.LockOSThread() }
+
+func main() {
+	vm := window.NewSfVideoMode()
+	defer window.DeleteSfVideoMode(vm)
+	vm.SetWidth(800)
+	vm.SetHeight(600)
+	vm.SetBitsPerPixAddedAddedel(32)
+
+	/* Create the main window */
+	cs := window.NewSfContextSettings()
+	defer window.DeleteSfContextSettings(cs)
+	w := graphics.SfRenderWindow_create(vm, "SFML window", uint(window.SfResize|window.SfClose), cs)
+	defer window.SfWindow_destroy(w)
+
+	ev := window.NewSfEvent()
+	defer window.DeleteSfEvent(ev)
+
+	/* Start the game loop */
+	for window.SfWindow_isOpen(w) > 0 {
+		/* Process events */
+		for window.SfWindow_pollEvent(w, ev) > 0 {
+			/* Close window: exit */
+			if ev.GetXtype() == window.SfEventType(window.SfEvtClosed) {
+				return
+			}
+		}
+		graphics.SfRenderWindow_clear(w, graphics.GetSfRed())
+		graphics.SfRenderWindow_display(w)
+	}
+}
