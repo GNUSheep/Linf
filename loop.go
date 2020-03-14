c8c63e33e37a2a8c1198baaae2c23746551e7d9d
Qiu3344 <qiu@qiu.news>
2020-03-14 14:56:13 +0100

    Removed rendering in a seperate thread and now you can press escape to exit

--- loop.go
+++ loop.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"gopkg.in/teh-cmc/go-sfml.v24/window"
+	"os"
 )
 
 // Global sfEvent varible
@@ -10,23 +11,26 @@ var eve window.SfEvent
 var gs string
 
 func game_loop() {
+
 	eve = window.NewSfEvent()
 	defer window.DeleteSfEvent(eve)
 	gs = "menu"
 
 	/* Start the render loop */
-	go render()
+	// go render()
 	
 	/* Start the game loop */
-	for window.SfWindow_isOpen(win) > 0 {
+	for true {
 		/* Process events */
 		switch gs {
 		case "menu":
 			menu()
 		case "playing":
 			playing()
+		case "exit":
+			os.Exit(0)
 		default:
-			return
+			os.Exit(0)
 		}
 	}
 }
--- menu.go
+++ menu.go
@@ -3,16 +3,30 @@ package main
 import (
 	"gopkg.in/teh-cmc/go-sfml.v24/graphics"
 	"gopkg.in/teh-cmc/go-sfml.v24/window"
+	"os"
 )
 
 func menu() {
-	menu_events()
+	menu_loop()
 }
+
+func menu_loop() {
+	for window.SfWindow_isOpen(win) > 0 {
+		menu_events()
+		menu_render()
+	}
+}
+
 func menu_events() {
 	for window.SfWindow_pollEvent(win, eve) > 0 {
 		/* Close window: exit */
 		if eve.GetXtype() == window.SfEventType(window.SfEvtClosed) {
-			return
+			os.Exit(0)
+		}
+		if eve.GetXtype() == window.SfEventType(window.SfEvtKeyPressed) {
+			if (int(eve.GetKey().GetCode()) == window.SfKeyEscape) {
+				os.Exit(0)
+			}
 		}
 	}
 }
