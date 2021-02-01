936f022ea915209f54193a35246e17cd5790a3a4
Q-I-U <qiu@qiu.news>
2021-02-01 23:47:53 +0100

    Add test button

--- gui.go
+++ gui.go
@@ -1,19 +1,23 @@
 package main
 
 import (
-	// "github.com/veandco/go-sdl2/sdl"
+	"github.com/veandco/go-sdl2/sdl"
+	"github.com/veandco/go-sdl2/gfx"
 )
 
-type Element interface {
-	render() bool
-	handleInput() bool
+const border = 5
+
+type Button struct {
+	x, y int32
+	width, height int32
+	text string
+	bgColor sdl.Color
+	fgColor sdl.Color
 }
 
-type ObjSystem struct {
-	elements []Element
-}
-
-func (o ObjSystem) addElement(e Element, name string) {
-	o.elements = append(o.elements, e)
-	
+func (b Button) draw(renderer *sdl.Renderer) {
+	gfx.BoxColor(renderer, b.x, b.y, b.x+b.width, b.y+b.height, b.fgColor)
+	gfx.BoxColor(renderer, b.x+border, b.y+border,
+		b.x+b.width-border, b.y+b.height-border, b.bgColor)
+	gfx.StringColor(renderer, b.x+b.width/2, b.y+b.height/2, "GFX Demo", sdl.Color{255, 0, 0, 255})
 }
--- main.go
+++ main.go
@@ -1,29 +1,36 @@
 package main
 
 import (
+	"github.com/veandco/go-sdl2/gfx"
 	"github.com/veandco/go-sdl2/sdl"
 )
 
-var window *sdl.Window
-var renderer *sdl.Renderer
+var winTitle string = "Linf"
+var winWidth, winHeight int32 = 800, 600
+
 func main() {
+	var window *sdl.Window
+	var renderer *sdl.Renderer
 	sdl.Init(sdl.INIT_EVERYTHING)
 	defer sdl.Quit()
 
-	window, _ = sdl.CreateWindow("Linf", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-		800, 600, sdl.WINDOW_SHOWN)
+	window, _ = sdl.CreateWindow(winTitle, sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, winWidth, winHeight, sdl.WINDOW_SHOWN)
 	defer window.Destroy()
+
 	renderer, _ = sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
 	renderer.Clear()
 	defer renderer.Destroy()
 
-	for true {
-		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-			switch event.(type) {
-			case *sdl.QuitEvent:
-				println("Quit")
-				break
-			}
-		}
-	}
+	button := Button{ 
+		x: 0, y: 0, width: 100, height: 100, text: "test", 
+		bgColor: sdl.Color{0, 255, 0, 255},
+		fgColor: sdl.Color{255, 255, 255, 255}}
+	button.draw(renderer)
+
+	gfx.CharacterColor(renderer, winWidth-16, 16, 'X', sdl.Color{255, 0, 0, 255})
+	gfx.StringColor(renderer, 16, 16, "GFX Demo", sdl.Color{0, 255, 0, 255})
+
+	renderer.Present()
+	sdl.Delay(3000)
+
 }
