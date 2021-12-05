50f2cd29bcd31cd1962ddbdaf6a9b610f5823f03
qiu-x <alex@alexslomka.xyz>
2021-12-05 23:30:24 +0100

    Remove debug log on button press

--- gui.go
+++ gui.go
@@ -5,8 +5,6 @@ import (
 	"github.com/veandco/go-sdl2/gfx"
 	sdlttf "github.com/veandco/go-sdl2/ttf"
 	"github.com/veandco/go-sdl2/img"
-	"fmt"
-	// "reflect"
 )
 
 const border = 5
@@ -49,13 +47,10 @@ func (b *Button) draw(renderer *sdl.Renderer) {
 func (b *Button) handleInput(e sdl.Event) {
 	switch t := e.(type) {
 	case *sdl.MouseButtonEvent:
-		// fmt.Printf("[%d ms] MouseButton\ttype:%d\tid:%d\tx:%d\ty:%d\tbutton:%d\tstate:%d\n",
-		// t.Timestamp, t.Type, t.Which, t.X, t.Y, t.Button, t.State)
 		if b.x-(b.width/2) < t.X && t.X < b.x+(b.width/2) &&
 		   b.y-(b.height/2) < t.Y && t.Y < b.y+(b.height/2) {
 			switch t.State {
 			case 1:
-				fmt.Println(b)
 				b.bgColor.A = 150
 			case 0:
 				b.onClick()
